e9a7aad2ffef4c293114f0282ee1658181e15504
PhilipChakram <philip.chakram@gmail.com>
2019-10-14 10:13:27 -0400

    Run as root

--- scripts/hungry-services-staging/before-install.sh
+++ scripts/hungry-services-staging/before-install.sh
@@ -1,5 +1,9 @@
 if [ "$DEPLOYMENT_GROUP_NAME" = "hungry-services-auth" ]
 then
     echo $DEPLOYMENT_GROUP_NAME
-    git clone -b staging ssh://git@github.com:tryhungry/hungry-services.git /home/deploy/hungry_services
+    aws s3 cp s3://hungry-resource/github-production.pem /root/.ssh/github-production.pem
+    eval $(ssh-agent -s)
+    ssh-add /root/.ssh/github-production.pem
+    rm -rf /home/deploy/hungry-services
+    git clone -b feature/user-service ssh://git@github.com/tryhungry/hungry-services.git /home/deploy/hungry-services
 fi
