b5802e35cdacb1b73adffc111d0c7ff7679d10d7
PhilipChakram <philip.chakram@gmail.com>
2019-10-14 12:45:41 -0400

    Change permissions on pem file

--- scripts/hungry-services-staging/before-install.sh
+++ scripts/hungry-services-staging/before-install.sh
@@ -3,6 +3,7 @@ if [ "$DEPLOYMENT_GROUP_NAME" = "hungry-services-auth" ]
 then
     echo $DEPLOYMENT_GROUP_NAME
     aws s3 cp s3://hungry-resource/github-production.pem /root/.ssh/github-production.pem
+    chmod 400 /root/.ssh/github-production.pem
     eval $(ssh-agent -s)
     ssh-add /root/.ssh/github-production.pem
     rm -rf /home/deploy/hungry-services
