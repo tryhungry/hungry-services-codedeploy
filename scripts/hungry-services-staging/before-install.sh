0e7eae5dd99dd0becfd41823ea2f7f2e347a678a
PhilipChakram <philip.chakram@gmail.com>
2019-10-13 20:57:01 -0400

    Use full path for aws

--- scripts/hungry-services-staging/before-install.sh
+++ scripts/hungry-services-staging/before-install.sh
@@ -1,7 +1,7 @@
 if [ "$DEPLOYMENT_GROUP_NAME" = "hungry-services-auth" ]
 then
     echo $DEPLOYMENT_GROUP_NAME
-    aws s3 cp s3://hungry-resource/github-production.pem /root/.ssh/github-production.pem
+    /home/ubuntu/.local/bin/aws s3 cp s3://hungry-resource/github-production.pem /root/.ssh/github-production.pem
     ssh-add /root/.ssh/github-production.pem
     git clone -b staging ssh://git@github.com:tryhungry/hungry-services.git /home/deploy/hungry_services
 fi
